ca7467e5bbe982e0cfe58bbf5299e64a2417f9a1
arquivosaxial-hue <arquivosaxial@gmail.com>
2026-08-31 21:56:02 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker - DTS
 // Permite que o app funcione offline após a primeira abertura com internet.
-const CACHE = "dts-axial-v14";
+const CACHE = "dts-axial-v15";
 
 // Arquivos do próprio app (mesma origem) para pré-cachear na instalação
 const APP_SHELL = [
